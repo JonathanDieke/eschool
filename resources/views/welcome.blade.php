13e3eafd1465eaee7885b463532f2f200b378fcc
JonathanDieke <louloulexus@gmail.com>
2021-01-20 19:46:59 +0000

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" charset="ascii" />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
